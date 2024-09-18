a75a1196c7dab613ba5aebcc472c6d3362f689b2
neruneruna7 <neruneruna7@gmail.com>
2024-09-18 21:44:13 +0900

    ユーザー作成 Fixes #1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use axum::{routing::get, Router};
+use shuttle_runtime::SecretStore;
+
+mod endpoints;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
 #[shuttle_runtime::main]
-async fn main() -> shuttle_axum::ShuttleAxum {
+async fn main(
+    #[shuttle_runtime::Secrets] _secrets: SecretStore
+) -> shuttle_axum::ShuttleAxum {
     let router = Router::new().route("/", get(hello_world));
 
     Ok(router.into())
