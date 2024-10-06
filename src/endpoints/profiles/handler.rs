b981bedfe9e5ddd48715d195672b513cb37a2409
neruneruna7 <neruneruna7@gmail.com>
2024-10-06 11:02:35 +0900

    update: get profile handler

--- src/endpoints/profiles/handler.rs
+++ src/endpoints/profiles/handler.rs
@@ -1,12 +1,17 @@
 use std::{collections::HashMap, num::ParseIntError};
 
-use axum::{extract::Path, http::StatusCode, routing::post, Extension, Json, Router};
+use axum::{
+    extract::Path,
+    http::StatusCode,
+    routing::{get, post},
+    Extension, Json, Router,
+};
 use tracing::info;
 
 use crate::{
     endpoints::{profiles::dto::Profile, users::dao_trait::DynUsersDao},
     error::{ConduitError, ConduitResult},
-    extractor::RequiredAuth,
+    extractor::{OptionalAuth, RequiredAuth},
 };
 
 use super::{dao_trait::DynProfilesDao, dto::ProfileRes};
@@ -26,6 +31,7 @@ impl ProfileRouter {
     pub fn to_router(&self) -> Router {
         Router::new()
             .route("/profiles/:username/follow", post(Self::follow_user))
+            .route("/profiles/:username", get(Self::get_profile_by_username))
             .layer(Extension(self.dyn_users_dao.clone()))
             .layer(Extension(self.dyn_profiles_dao.clone()))
     }
@@ -71,12 +77,34 @@ impl ProfileRouter {
         Path(params): Path<HashMap<String, String>>,
         Extension(users): Extension<DynUsersDao>,
         Extension(profiles): Extension<DynProfilesDao>,
+        OptionalAuth(current_user_id): OptionalAuth,
     ) -> ConduitResult<(StatusCode, Json<ProfileRes>)> {
         let user_name = params
             .get("username")
             .ok_or(ConduitError::NotFound(String::from("invalid param")))?;
         info!("received req: get profile by username: {}", user_name);
 
-        todo!()
+        // usernameからユーザー情報を取得
+        let user_entity = users.get_user_by_username(user_name).await?;
+        let Some(user_entity) = user_entity else {
+            return Err(ConduitError::NotFound("profile not found".to_string()));
+        };
+
+        // current_user_idがある場合，フォローしているかどうかを取得
+        let is_following = if let Some(user_id) = current_user_id {
+            let is_following = profiles
+                .get_user_following(user_entity.id)
+                .await?
+                .iter()
+                .any(|f| f.follower_id == user_id);
+            is_following
+        } else {
+            false
+        };
+
+        let profile = Profile::from_user_entity(user_entity, is_following);
+        let profile_res = ProfileRes { profile };
+
+        Ok((StatusCode::OK, Json(profile_res)))
     }
 }
