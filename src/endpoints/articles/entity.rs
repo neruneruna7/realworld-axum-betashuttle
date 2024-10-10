436c40aaf8eb739d00a1d89be2dcf6476222ef92
neruneruna7 <neruneruna7@gmail.com>
2024-10-10 17:26:22 +0900

    add: ArticleEntity

---
+++ src/endpoints/articles/entity.rs
@@ -0,0 +1,13 @@
+use sqlx::{prelude::FromRow, types::time::PrimitiveDateTime};
+use uuid::Uuid;
+
+#[derive(FromRow, Debug, Clone, PartialEq)]
+pub struct ArticleEntity {
+    pub id: i32,
+    pub created_at: PrimitiveDateTime,
+    pub updated_at: PrimitiveDateTime,
+    pub title: String,
+    pub description: String,
+    pub body: String,
+    pub author_id: Uuid,
+}
