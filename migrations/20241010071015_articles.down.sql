a6583f19b3b727833095a57ff70aa962adf932cc
neruneruna7 <neruneruna7@gmail.com>
2024-10-12 19:26:00 +0900

    add: タグとタグ-記事間の連関エンティティ

--- migrations/20241010071015_articles.down.sql
+++ migrations/20241010071015_articles.down.sql
@@ -1,2 +1,4 @@
 -- Add down migration script here
 DROP TABLE IF EXISTS articles;
+DELETE TABLE IF EXISTS tags;
+DELETE TABLE IF EXISTS article_tags;
--- migrations/20241010071015_articles.up.sql
+++ migrations/20241010071015_articles.up.sql
@@ -19,3 +19,20 @@ CREATE TRIGGER update_articles_modtime
 BEFORE UPDATE ON articles
 FOR EACH ROW
 EXECUTE PROCEDURE update_timestamp();
+
+
+-- タグテーブル
+CREATE TABLE IF NOT EXISTS tags (
+  id SERIAL PRIMARY KEY,
+  tag VARCHAR(255) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- タグを記事をつなぐ連関エンティティ
+CREATE TABLE IF NOT EXISTS article_tags (
+  article_id SERIAL NOT NULL,
+  tag_id SERIAL NOT NULL,
+  PRIMARY KEY (article_id, tag_id),
+  FOREIGN KEY (article_id) REFERENCES articles(id) ON DELETE CASCADE,
+  FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
+);
