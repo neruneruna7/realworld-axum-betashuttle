b25e0e3693a47931b4a1b1507b917a177928db2e
neruneruna7 <neruneruna7@gmail.com>
2024-10-14 22:05:51 +0900

    いいね作成 Fixes #35

---
+++ migrations/20241014125500_favorites.up.sql
@@ -0,0 +1,10 @@
+-- Add up migration script here
+CREATE TABLE IF NOT EXISTS favorites (
+  user_id UUID NOT NULL,
+  article_id SERIAL NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  deleted BOOLEAN NOT NULL DEFAULT FALSE,
+  PRIMARY KEY (user_id, article_id),
+  FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+  FOREIGN KEY (article_id) REFERENCES articles(id) ON DELETE CASCADE
+);
